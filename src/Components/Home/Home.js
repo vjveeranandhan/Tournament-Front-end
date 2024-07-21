e4429673c99c99d289f76cbfb423dffc948da6de
vjveeranandhan <vjveeranandhan@gmail.com>
2024-07-21 02:10:14 +0200

    style changes in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,14 +9,20 @@ const ComponentWithBackgroundImage = () => {
         <h2 className='container-2-h2'>Unleash Your Potential Enter the Arena of Champions!!</h2>
       </div>
       <div className='container-3'>
+        <div>
         <h2 className='container-3-h2'>Craft your ultimate tournament experience</h2>
-        <button>Host</button>
+        </div>
+        <div>
+        <button className="new-line-button" >Host</button>
+        </div>
       </div>
       <div className='container-4'>
-        <h2 className='container-4-h2'>Form your dream
-        <h1 className='container-4-h2-h1-1'>Team</h1>
-        from scratch</h2>
-        <button>Build</button>
+        <div className='container-4-div-1'>
+          <h2 className='container-4-h2'>Form your dream
+          <h1 className='container-4-h2-h1-1'>Team</h1>
+          from scratch</h2>
+          <button>Build</button>
+        </div>
         <img src={players} alt="Example" className="image" />
       </div>
       <div className='container-5'>
